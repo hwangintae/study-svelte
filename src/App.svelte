7d09882a5917a0754a4a9090cac2dd6eccda441c
intaeHwang <hwang9029@ckinform.com>
2025-05-12 00:06:02 +0900

    feat/ 클릭횟수 구현

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,10 @@
 <script>
-	export let name;
+	let num = 0;
+	$: square = num * num;
+	const incrementCount = () => num = num + 1;
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<button on:click={incrementCount}>
+	클릭횟수 : {num}
+</button>
+<p>제곱 값: {square}</p>
