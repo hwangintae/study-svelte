417dd2aead38596428987aefaa3920e3fdaa32fd
intaeHwang <hwang9029@ckinform.com>
2025-05-23 12:46:57 +0900

    feat/ props 기본값

--- src/chapter07/Child01.svelte
+++ src/chapter07/Child01.svelte
@@ -1,7 +1,7 @@
 <script>
-    export let name;
-    export let age;
-    export let hobby;
+    export let name = "우직이";
+    export let age = 1;
+    export let hobby = "깨물기";
 </script>
 
 <h3>이름: {name}</h3>
--- src/chapter07/Parent01.svelte
+++ src/chapter07/Parent01.svelte
@@ -4,3 +4,4 @@
 
 <Child01 name={'황인태'} age={31} hobby={'잠자기'} />
 <Child01 name="박수현" age="30" hobby="눕기" />
+<Child01 />
