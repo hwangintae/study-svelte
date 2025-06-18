f0c020a83bb3b76cfe1ee1b2226c507357ebeaa8
intaeHwang <hwang9029@ckinform.com>
2025-06-18 21:46:22 +0900

    feat/ slot name 설정해서 전달하기

---
+++ src/chapter11/SlotParent02.svelte
@@ -0,0 +1,12 @@
+<script>
+    import Child02 from "./Child02.svelte";
+</script>
+<Child02>
+    <span slot="name">스벨트(Svelte)</span>
+    <span slot="release">2016</span>
+</Child02>
+<Child02>
+    <span slot="name">리액트(React)</span>
+    <span slot="release">2013</span>
+</Child02>
+<Child02></Child02>
