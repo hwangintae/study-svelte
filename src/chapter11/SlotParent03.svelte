6658a6a4cd167c99f524612a04f3d51f891c1389
intaeHwang <hwang9029@ckinform.com>
2025-06-19 12:56:57 +0900

    feat/ 자식 component에서 slot을 이용하여 부모 component에 상태값 전달하기

---
+++ src/chapter11/SlotParent03.svelte
@@ -0,0 +1,19 @@
+<script>
+    import Child03 from "./Child03.svelte";
+</script>
+
+<Child03 let:hovering={active}>
+    <span class:active={active}>스벨트(Svelte)</span>
+</Child03>
+
+<Child03 let:hovering={active}>
+    <span class:active={active}>리액트(React)</span>
+</Child03>
+
+<style>
+    .active {
+        background-color: #000;
+        color: #fff;
+        cursor: pointer;
+    }
+</style>
