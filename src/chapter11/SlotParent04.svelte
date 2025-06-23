2d1bda161846d3dcbbc328abc477c306e1673374
intaeHwang <hwang9029@ckinform.com>
2025-06-23 12:29:33 +0900

    feat/ 조건 slot을 이용한 성능 최적화

---
+++ src/chapter11/SlotParent04.svelte
@@ -0,0 +1,15 @@
+<script>
+    import Child04 from "./Child04.svelte";
+</script>
+<Child04>
+    <span slot="name">스벨트(Svelte)</span>
+    <span slot="release">2016</span>
+</Child04>
+<Child04>
+    <span slot="name">리엑트(React)</span>
+    <span slot="release">2013</span>
+</Child04>
+<Child04>
+    <span slot="name">뷰(Vue.js)</span>
+</Child04>
+<Child04></Child04>
