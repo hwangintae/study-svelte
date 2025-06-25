b9d2e4116085fcf3b439f400e6f4aaa3581dc2d9
intaeHwang <hwang9029@ckinform.com>
2025-06-25 12:29:50 +0900

    feat/ onMount를 이용하여 init

---
+++ src/chapter11/OnMount.svelte
@@ -0,0 +1,32 @@
+<script>
+    import {onMount} from "svelte";
+
+    let comments = [];
+
+    onMount(async () => {
+        const res = await fetch("https://jsonplaceholder.typicode.com/comments?_limit=21");
+        comments = await res.json();
+    });
+</script>
+
+<style>
+    .comments {
+        width: 100%;
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        grid-gap: 8px;
+    }
+</style>
+
+<h3>회원 정보</h3>
+
+<div class="comments">
+    {#each comments as comment}
+        <article>
+            <h4>이름 : {comment.name}</h4>
+            <h4>이메일 주소 : {comment.email}</h4>
+        </article>
+    {:else}
+        <p>loading...</p>
+    {/each}
+</div>
