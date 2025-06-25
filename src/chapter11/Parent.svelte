08279792d8ccf8df6cb7a4c572ed1019a5bfce46
intaeHwang <hwang9029@ckinform.com>
2025-06-25 12:30:12 +0900

    feat/ onMount, onDestroy 비교

---
+++ src/chapter11/Parent.svelte
@@ -0,0 +1,19 @@
+<script>
+    import Child05 from "./Child05.svelte";
+
+    let isChildElementView = false;
+
+    const handleClick = (bool) => {
+        isChildElementView = bool;
+    }
+</script>
+
+<h2> 부모 component </h2>
+
+<button on:click={() => handleClick(true)}>Child component 활성화 </button>
+<button on:click={() => handleClick(false)}>Child component 비활성화 </button>
+<hr />
+
+{#if isChildElementView}
+    <Child05 />
+{/if}
