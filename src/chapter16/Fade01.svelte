bb8f701070769b559b03428cf5b34e8836b44cf8
intaeHwang <hwang9029@ckinform.com>
2025-08-08 17:27:08 +0900

    feat/ fade 추가

---
+++ src/chapter16/Fade01.svelte
@@ -0,0 +1,12 @@
+<script>
+    import {fade} from 'svelte/transition'
+    let visible = false;
+</script>
+
+<label>
+    <input type="checkbox" bind:checked={visible} /> 보임
+</label>
+
+{#if visible}
+    <p transition:fade>Svelte Fade Effect</p>
+{/if}
