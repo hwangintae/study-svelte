d1a2d6d5cef767fc1e107f05fb49931d85ea8266
intaeHwang <hwang9029@ckinform.com>
2025-08-27 23:16:34 +0900

    feat/ 16.3 예제

---
+++ src/chapter16/Blur03.svelte
@@ -0,0 +1,23 @@
+<script>
+    import {blur} from 'svelte/transition';
+
+    let visible = false;
+</script>
+
+<label>
+    <input type="checkbox" bind:checked={visible}/>보임
+</label>
+
+{#if visible}
+    <p
+            in:blur={{amount: 5}}
+            out:blur={{amount: 20}}
+    >Svelte Blur Effect</p>
+{/if}
+
+<style>
+    p {
+        height: 100px;
+        background-color: orange;
+    }
+</style>
