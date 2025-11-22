a3f28d2c0da6659b70af0470822451b12a930e24
intaeHwang <hwang9029@ckinform.com>
2025-11-22 20:11:12 +0900

    버킷리스트 구현

---
+++ src/components/BucketHeader.svelte
@@ -0,0 +1,19 @@
+<script>
+    const today = new Date();
+    const week = ["일", "월", "화", "수", "목", "금", "토"];
+
+    const dateString = {
+        year: today.getFullYear(),
+        month: today.getMonth() + 1,
+        date: today.getDate(),
+        day: today.getDay()
+    };
+
+    const { year, month, date, day } = dateString;
+</script>
+
+<div class="bucketheader">
+    <h1>My Bucket List</h1>
+    <h2>현재: {year}년 {month}월 {date}일 {week[day]}요일</h2>
+    <p>할 일 3개 남음</p>
+</div>
